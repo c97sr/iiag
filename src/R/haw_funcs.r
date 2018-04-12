d1b18ea7b79ba30ed648812942945df64da7a0cf
dhaw <d.haw@imperial.ac.uk>
2018-04-12 11:56:37 +0100

    SARIMA added

--- src/R/haw_funcs.r
+++ src/R/haw_funcs.r
@@ -336,6 +336,142 @@ p
 
 ################################################################################################################################
 
+predictFlu <- function(dfNet, dfID, incidenceMeasure, zones, nameArea, plotACF, 
+                       numWeeks, thisFar, recentData, 
+                       arima1, arima2, arima3, 
+                       seasonal1, seasonal2, seasonal3, seasonPeriod
+){#, plotCategory, YearStart){
+  
+  options(warn=-1)
+  #Country only for now:
+  cName <- nameArea
+  countryName <- zones$country[which(zones$ISO3==cName)]
+  countryName <- countryName[1]
+  
+  if (incidenceMeasure=="sero"){
+    dfx <- dfNet
+    fluid1c <- dfx[dfx$ISO3==cName, ]
+  }else{
+    dfx <- dfID
+    fluid1c <- dfx[dfx$ISO3==cName, ]
+    fluid1c <- fluid1c[fluid1c$AGEGROUP_CODE=="All", ]
+  }
+  #
+  if (incidenceMeasure=="sero"){
+    df <- dfx[which(dfx$MEASURE_CODE=="SPEC_PROCESSED_NB" |
+                      dfx$VIRUSTYPE_CODE=="ALL_INF"), ]
+    df2 <- df[df$ISO3==cName, ]
+    df2 <- data.frame(df2$ISO_YEAR, df2$ISO_WEEK, df2$MEASURE_CODE, df2$ValueNumeric)
+    colnames(df2) <- c("year", "week", "code", "value")
+    #### Reshape data so there is one row per week.
+    df2 <- reshape(df2, idvar=c("year", "week"), timevar=c("code"), direction="wide")
+    #Should land in chronological order ********
+    #### Use perventage positive (3 point moving average) as measure of incidence:
+    df2$percPos <- df2[, 3]/df2[, 4]*100
+    #df2$percPosMov <- rollapply(df2$percPos, 3, mean, align="center", fill=T)
+    df2 <- df2[, c(1,2,5)]
+    names(df2)[1:3] <- paste(c("year", "week", "value"))
+    fluid1is <- aggregate(df2$value, by=list(df2$year, df2$week), FUN=sum, na.rm=TRUE)
+    names(fluid1is)[1:3] <- paste(c("year", "week", "value"))
+    #
+  }else{
+    #Extract cases:
+    toMatch <- c("ILI_CASES", "ILI_OUTPATIENTS", "SARIC_CASES", "SARI_DEATHS$", "SARI_INPATIENTS", "SARI_DEATHS_INPATIENTS")#All usable
+    #toMatch <- c("ILI_CASES", "SARI _CASES")
+    #toMatch <- c("DEATHS", "PATIENTS$")
+    fluidc <- fluid1c[grep(paste(toMatch, collapse="|"), fluid1c$MEASURE_CODE),]
+    #
+    #fluidc <- data.frame(fluidc$YEAR_CODE,fluidc$WEEK_CODE,fluidc$MEASURE_CODE,fluidc$AGEGROUP_CODE,fluidc$DataValue)
+    #Why doesn't work? - get "fluidc." in column names
+    keeps <- c("ISO_YEAR","ISO_WEEK","MEASURE_CODE","AGEGROUP_CODE","ValueNumeric")
+    fluidc <- fluidc[keeps]
+    names(fluidc)[1:5] <- paste(c("year", "week", "case", "age", "value"))#case
+    #
+    fluidc$week <- as.numeric(fluidc$week)
+    fluidc$year <- as.numeric(fluidc$year)
+    fluidc$value <- as.numeric(as.character(fluidc$value))
+    #fluisc <- fluidc[order(fluidc$week),]
+    #fluisc <- order(fluidc$year),]#Retains sub-order from previous? #Col numbers retain old order
+    #
+    #Separate out ILI and SARI CASES only:
+    cases <- c("ILI_CASES", "SARI_CASES")#Include sub-cases?
+    fluidis <- fluidc[grep(paste(cases, collapse="|"), fluidc$case),]#"ILI/SARI"
+    #ILI+SARI:
+    fluid1is <- aggregate(fluidis$value, by=list(fluidis$year, fluidis$week), FUN=sum, na.rm=TRUE)
+    names(fluid1is)[1:3] <- paste(c("year", "week", "value"))
+  }
+  #
+  ##From here, the term "fluid" appears in variable names, even if data is fluNet. 
+  ##This is because code was initially written with just fluID in mind.
+  #
+  #Order correctly:
+  fluid1is <- fluid1is[order(fluid1is$week), ]
+  fluid1is <- fluid1is[order(fluid1is$year), ]
+  if (plotACF==TRUE){
+    ##ACF and PACF in same figure:
+    forACF <- forACF <- fluid1is$value
+    par(mfrow=c(2,1))
+    acf(forACF, main="Data: ACF")#, main="Autocorrelation function")
+    #grid(nx = NULL, ny = nx, col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
+    pacf(forACF, main="Data: PACF")#, main="Partial autocorrelation function")
+    #grid(nx = NULL, ny = nx, col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
+  }
+  #
+  #Disguard most recent:
+  lf <- length(fluid1is$value)
+  if(lf < recentData) stop("Not enough data: predict shorter time period")
+  realInd <- seq(lf-recentData+1, lf)
+  REAL <- fluid1is[realInd, ]
+  fluid1is <- fluid1is[-realInd, ]
+  #
+  disguardStart <- lf-recentData-numWeeks
+  if (disguardStart>0){
+    fluid1is <- fluid1is[-c(1:disguardStart), ]
+  }
+  lf <- length(fluid1is$value)
+  #
+  #ARIMA:
+  fluid.fit <- arima(fluid1is$value, order=c(arima1,arima2,arima3), seasonal=list(order=c(seasonal1,seasonal2,seasonal3), period=seasonPeriod), include.mean=FALSE)
+  #print(fluid.fit)
+  if (plotACF==TRUE){
+    ##ACF and PACF in same figure:
+    forACF <- fluid.fit$residuals
+    par(mfrow=c(2,1))
+    acf(forACF, main="Model: ACF")#, main="Autocorrelation function")
+    #grid(nx = NULL, ny = nx, col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
+    pacf(forACF, main="Model: PACF")#, main="Partial autocorrelation function")
+    #grid(nx = NULL, ny = nx, col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
+  }
+  
+  #Predictions:
+  fluid.pred <- predict(fluid.fit, n.ahead=thisFar)
+  xOld <- seq(1, lf)
+  xReal <- seq(lf+1, lf+length(REAL$value))
+  xNew <- seq(lf+1, lf+thisFar)
+  y1 <- fluid.pred$pred-2*fluid.pred$se
+  y2 <- fluid.pred$pred+2*fluid.pred$se
+  xMax <- max(max(xOld), max(xReal))
+  if (incidenceMeasure=="sero"){
+    yMax <- 100
+    yLabel <- "Percentage positive"
+  }else{
+    yMax <- max(max(fluid1is$value), max(y2))
+    yLabel <- "Cases"
+  }
+  redTrans <- rgb(255, 0, 0255, max = 255, alpha = 50)
+  
+  par(mfrow=c(1,1))
+  plot(xOld, fluid1is$value, type="l", xlim=c(0, xMax), ylim=c(0, yMax), xlab="Time (weeks)", ylab=yLabel, main=countryName, lwd=2)
+  polygon(c(xNew,rev(xNew)),c(y2,rev(y1)),col=redTrans)
+  lines(xNew, y2, col="red", lwd=2)
+  lines(xNew, y1, col="red", lwd=2)
+  lines(fluid.pred$pred, col="blue", lwd=2)
+  lines(xReal, REAL$value, col="black", lty = "dotted", lwd=2)
+  grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
+}
+
+################################################################################################################################
+
 if (FALSE) {
   
   ## You may need to change this for where you have downloaded the git repository:
@@ -357,7 +493,7 @@ if (FALSE) {
   
   ## Assumes running in src/R. Change datadir as needed, otherwise leave this bit:
   tmp <- load.iiag.data(datadir="data")
-  df <- tmp$lab
+  dfNet <- tmp$lab
   dfID <- tmp$synd
   zones <- read.csv("data/CountryList.csv")
   zones <- data.frame(lapply(zones, as.character), stringsAsFactors=FALSE)
@@ -366,6 +502,7 @@ if (FALSE) {
   zones$hemis[zones$hemis=="Southern hemisphere"] <- "southern"
   
   ################################
+  # AVERAGE CURVES:
   # INPUTS - if above has run once, just run from here.
   
   ## For average curve:
@@ -373,10 +510,11 @@ if (FALSE) {
   nameArea <- "USA" #Select country (ISO3) or ITZ (ITZ name as in table)
   YearStart = 2009 #First year of required season
   ## Plot desired average curve:
-  average_curve(df, zones, plotCategory, nameArea, YearStart)
+  average_curve(dfNet, zones, plotCategory, nameArea, YearStart)
   
   
   #################################
+  # SUBTYPES BY YEAR/SEASON:
   
   ## Option to plot distribution of subtypes by year:
   ## Comment out next 5 lines if no bar plot desired.
@@ -385,4 +523,38 @@ if (FALSE) {
   #splitOther <- FALSE #Divide non-H1/H3 between these 2 (for "tidier" plots)
   #prepStrain(df, dfID, zones, proportion)
   #extractCountry(dfout, iso3, splitOther)
+  
+  ################################
+  # SARIMA:
+  # INPUTS - if above has run once, just run from here.
+  
+  ## For SARIMA:
+  #plotCategory <- "country" #"zone" (ITZ) or "country" - add whoregion?
+  nameArea <- "USA" #Select country (ISO3) or ITZ (ITZ name as in table)
+  numYears <- 4
+  numWeeks <- 52*numYears #Total number of weeks to use
+  recentData <- 20 #Remove most recent weeks
+  #Predict this many weeks:
+  thisFar <- 28
+  incidenceMeasure <- "case"#"sero" or "case"
+  #
+  #(S)ARIMA parameters:
+  arima1 <- 1 #AR
+  arima2 <- 1 #Diff
+  arima3 <- 0 #MA
+  seasonal1 <- 1 #Seasonal AR
+  seasonal2 <- 1 #Seasonal diff
+  seasonal3 <- 1 #Seasonal MA
+  seasonPeriod <- 52 #Weeks - 52, 26 for tropics?
+  #
+  plotACF <- TRUE
+  #Run the function:
+  predictFlu(dfNet, dfID, incidenceMeasure, zones, nameArea, plotACF, 
+             numWeeks, thisFar, recentData, 
+             arima1, arima2, arima3, 
+             seasonal1, seasonal2, seasonal3, seasonPeriod)
+  ##Message from Sarah Kramer:
+  #The 18 countries I considered as "tropical" (and that had decent-quality syndromic and virologic data) were:
+  #Brazil, Bolivia, Colombia, Cuba, Ecuador, Honduras, Paraguay, Peru, Kenya, Madagascar, 
+  #Oman, Pakistan, Bangladesh, Bhutan, Cambodia, Indonesia, Singapore, Thailand 
 }
