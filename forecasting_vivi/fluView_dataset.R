da99bfcc443184d99ca8a4b73846dfb098099414
haowei_wang <haowei.wang18@ic.ac.uk>
2019-12-11 18:17:37 +0000

    record work

--- forecasting_vivi/fluView_dataset.R
+++ forecasting_vivi/fluView_dataset.R
@@ -9,12 +9,18 @@ library("knitr")
 library("xgboost")
 library("stringr")
 
-source("E:/Imperial/iiag/forecasting_vivi/fluView_funcs_vivi.R")
+# source("E:/Imperial/iiag/forecasting_vivi/fluView_funcs_vivi.R")
+source("C:/Users/hw3616/Desktop/Imperial/Project1_Forecasting/Project_Coding/iiag/forecasting_vivi/fluView_funcs_vivi.R")
 
 #' U.S. Outpatient Influenza-like Illness Surveillance Network (ILINet) consist of 
 #' information on outpatient visits to health care providers for influenza-like illness
 #' and the number of outpatinent confirmed as ILI.
-# fluView.ILINet <- read.csv("C:/Users/hw3616/Desktop/Imperial/Project1_Forecasting/Project_Coding/iiag/forecasting_vivi/fluView_data/ILINet.csv")
-fview_ILINet <- read.csv("E:/Imperial/iiag/forecasting_vivi/fluView_data/ILINet.csv")
-fview_ILINet <- load.iiag.data.fluView("E:/Imperial/iiag/forecasting_vivi/fluView_data")
-#' First of all, beause 
+
+#' First of all, fix the headers of dataset to the form that won's cause error in the Rstudio
+fview_ILINet <- load.iiag.data.fluView("C:/Users/hw3616/Desktop/Imperial/Project1_Forecasting/Project_Coding/iiag/forecasting_vivi/fluView_data/")
+
+# fview_ILINet <- load.iiag.data.fluView("E:/Imperial/iiag/forecasting_vivi/fluView_data")
+
+#' Extract incidence data by states.
+
+
--- forecasting_vivi/fluView_funcs_vivi.R
+++ forecasting_vivi/fluView_funcs_vivi.R
@@ -2,23 +2,43 @@ load.iiag.data.fluView <- function(datadir="../fluView_data") {
   
   ## Helper function to fix some header names to be used below
   fix_headers <- function(x){
-    nchar <- sapply(strsplit(x, " "), length)
-    if (nchar > 1){
-      if (length(unlist(strsplit(x,"-"))) > 1){
-        x <- unlist(strsplit(unlist(strsplit(x," ")),"-"))
+    x <- as.character(x)
+    nchar <- unlist(strsplit(x, " "))
+    if (length(nchar) > 1){
+      if (length(unlist(strsplit(nchar, "."))) > 1){
+        x <- unlist(strsplit(nchar, ".", fixed = TRUE))
       }
+      
+      if (length(unlist(strsplit(nchar, "%", fixed = TRUE))) > 1){
+        x <- str_replace(x, "%", "PERCENTAGE OF ")
+        x <- unlist(strsplit(x, " "))
+      }
+      if (length(unlist(strsplit(nchar, "-"))) > 1){
+        x <- unlist(strsplit(nchar,"-"))
+      }
+    }else{
+      x <- nchar
     }
+
+    x <- paste0(x, collapse = "_")
+    x
   }
-  
   ## Define the strings for all the files needed and read 
  
   fview_ILINet <- read.csv(paste0(datadir,"/ILINet.csv"))
   
   ## Fix names for fluView ILINet
-  newnames <- unlist(fview_ILINet[1,], use.names=FALSE)
+  curnames <- unlist(fview_ILINet[1,], use.names=FALSE)
+  newnames <- c()
+  for (i in 1:length(curnames)){
+    tmp <- fix_headers(curnames[i])
+    newnames <- append(newnames,tmp)
+  }
+    
   names(fview_ILINet) <- newnames
-
   fview_ILINet <- fview_ILINet[-1,]
   
   fview_ILINet
 }
+
+#' Extract incidence 
